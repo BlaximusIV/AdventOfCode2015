2684b6eef9f1be9a8a1231499b0780a4f534d2fa
Neal Darley <darley.neal@gmail.com>
2021-02-19 09:19:56 -0700

    updating gitignore.

--- not_quite_lisp/target/debug/deps/not_quite_lisp-e11ef104548f393b.d
+++
@@ -1,5 +0,0 @@
-C:\repos\AdventOfCode2015\not_quite_lisp\target\debug\deps\not_quite_lisp-e11ef104548f393b.exe: src\main.rs
-
-C:\repos\AdventOfCode2015\not_quite_lisp\target\debug\deps\not_quite_lisp-e11ef104548f393b.d: src\main.rs
-
-src\main.rs:
--- not_quite_lisp/target/rls/debug/deps/not_quite_lisp-ce7be74012f3bc79.d
+++
@@ -1,5 +0,0 @@
-c:\repos\AdventOfCode2015\not_quite_lisp\target\rls\debug\deps\not_quite_lisp-ce7be74012f3bc79.rmeta: src\main.rs
-
-c:\repos\AdventOfCode2015\not_quite_lisp\target\rls\debug\deps\not_quite_lisp-ce7be74012f3bc79.d: src\main.rs
-
-src\main.rs:
