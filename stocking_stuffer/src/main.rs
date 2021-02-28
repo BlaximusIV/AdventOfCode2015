e3100973e6c17a492e011443e9eb1788d9859775
Neal Darley <darley.neal@gmail.com>
2021-02-28 07:26:21 -0700

    D4, pt 1. Brute force.

---
+++ stocking_stuffer/src/main.rs
@@ -0,0 +1,24 @@
+use regex::Regex;
+
+mod md5_hasher;
+
+fn main() {
+    const KEY: &str = "ckczppom";
+    let mut target_number = 0;
+
+    let re: Regex = Regex::new(r"^0{5}[0-9a-f]*").unwrap();
+    let mut hash_found = false;
+
+    while !hash_found {
+        let hash = md5_hasher::md5_utf8(&(KEY.to_owned() + &target_number.to_string()));
+
+        if re.is_match(&hash) {
+            hash_found = true;
+            break;
+        }
+
+        target_number += 1;
+    }
+
+    println!("Target found: {}", target_number);
+}
